addd69381d86788a68307f5ebb3810a309179d39
Deniss Ponomarenko <Deniss.Ponomarenko@sunpowercorp.com>
2022-04-24 13:44:02 +0300

    Add files via upload

--- Game1/Solution1/Script.js
+++ Game1/Solution1/Script.js
@@ -8,19 +8,24 @@ var last_button;
 who_will_start = confirm("If you want the computer to start the game, press OK");
 var active_player;
 
-if (who_will_start) {
-    active_player = 1;
+setTimeout(() => {
+  if (who_will_start) {
+    active_player = 'ai';
     console.log('Computer will start the game');
+	console.log('Map: ' + getButtonList());
+	robot_step( robot(getButtonList(), player1, player2, true));
     
 } else {
-    active_player = 2;
-    console.log('Player will start the game');
+    active_player = 'user';
+    console.log( user1 +' will start the game');
 }
+
+}, 2000)
 
 
 const myTimeout = setTimeout(change_color, 2000);
 
-function update_score(button) {
+function user_step(button) {
     
     last_button = (button.substring(0, 3));
 
@@ -43,14 +48,14 @@ function update_score(button) {
             btn1.innerHTML = '1';
             btn1.setAttribute('id', 'one6');
             btn1.classList = 'btn btn-success btn-lg m-1';
-            btn1.setAttribute('onclick', "update_score('one6')");
+            btn1.setAttribute('onclick', "user_step('one6')");
             document.getElementById('button_group').appendChild(btn1)
 
             let btn2 = document.createElement("button");
             btn2.innerHTML = '1';
             btn2.setAttribute('id', 'one7');
             btn2.classList = 'btn btn-success btn-lg';
-            btn2.setAttribute('onclick', "update_score('one7')");
+            btn2.setAttribute('onclick', "user_step('one7')");
             document.getElementById('button_group').appendChild(btn2)
         } else {
             log(last_button);
@@ -64,14 +69,14 @@ function update_score(button) {
             btn1.innerHTML = '1';
             btn1.setAttribute('id', 'one8');
             btn1.classList = 'btn btn-success btn-lg m-1';
-            btn1.setAttribute('onclick', "update_score('one8')");
+            btn1.setAttribute('onclick', "user_step('one8')");
             document.getElementById('button_group').appendChild(btn1)
 
             let btn2 = document.createElement("button");
             btn2.innerHTML = '2';
             btn2.setAttribute('id', 'two4');
             btn2.classList = 'btn btn-danger btn-lg';
-            btn2.setAttribute('onclick', "update_score('two4')");
+            btn2.setAttribute('onclick', "user_step('two4')");
             document.getElementById('button_group').appendChild(btn2)
         } else {
             log(last_button);
@@ -80,13 +85,14 @@ function update_score(button) {
     }
 
     document.getElementById(button).remove();
-    change_color();
     gemeOver();
+	change_color();
+	robot_step( robot(getButtonList(), player1, player2, true));
 }
 
 function set_score() {
     point = parseInt(last_button);
-    if (active_player == 1) {
+    if (active_player == 'ai') {
         player1 = player1 + point;
         document.getElementById("playerScore1").innerHTML = user1 + ": " + player1 + " points";
         console.log('Player1: ' + player1);
@@ -98,7 +104,7 @@ function set_score() {
 }
 
 function change_color() {
-    if (active_player == 1) {
+    if (active_player == 'ai') {
         var element = document.getElementById("playerBg1");
         element.classList.remove("bg-primary");
         element.classList.add("bg-secondary");
@@ -143,11 +149,11 @@ function gemeOver() {
 }
 
 function log(last_button) {
-    if (active_player == 1) {
-        active_player = 2;
+    if (active_player == 'ai') {
+        active_player = 'user';
         console.log('Robot scored ' + last_button + ' points');
     } else {
-        active_player = 1;
+        active_player = 'ai';
         console.log(user1 + ' scored ' + last_button + ' points');
     }
 }
@@ -155,108 +161,93 @@ function log(last_button) {
 function robot(buttonList, score1, score2, isMaximaxing) {
     if (buttonList.length == 0) {
         if (score1 > score2) {
-            return 1;
-        } else if(score1 < score2){
             return -1;
+        } else if(score1 < score2){
+            return -2;
         }else { return 0; }
     }
+	
+	take1Array = buttonList;
+	if (take1Array.indexOf('1') > -1) {
+		take1Array.splice(buttonList.indexOf('1'), 1);
+	}
+	
+	take2Array = buttonList;
+	if (take2Array.indexOf('1') > -1) {
+		take2Array.splice(buttonList.indexOf('1'), 1);
+	}
+	
+	take3Array = buttonList;
+	if (take3Array.indexOf('1') > -1) {
+		take3Array.splice(buttonList.indexOf('1'), 1);
+	}
+	
+	split3Array = buttonList;
+	if (split3Array.indexOf('3') > -1) {
+		split3Array.splice(buttonList.indexOf('3'), 1);
+		split3Array.push('2');
+		split3Array.push('1');		
+	}
+	
+	split2Array = buttonList;
+	if (split2Array.indexOf('2') > -1) {
+		split2Array.splice(buttonList.indexOf('2'), 1);
+		split2Array.push('1');
+		split2Array.push('1');		
+	}
+	
+	
     if (isMaximaxing) {
-        return Math.max(robot(getButtonList(3),  player1 + 3, player2, true),
-                        robot(getButtonList(2),  player1 + 2, player2, true),
-                        robot(getButtonList(1),  player1 + 1, player2, true),
-                        robot(getButtonList(-3), player1,     player2, true),
-                        robot(getButtonList(-2), player1,     player2, true)
-                )
-    } else {
-        return Math.min(robot(getButtonList(3),  player1, player2 + 3, false),
-                        robot(getButtonList(2),  player1, player2 + 2, false),
-                        robot(getButtonList(1),  player1, player2 + 1, false),
-                        robot(getButtonList(-3), player1, player2,     false),
-                        robot(getButtonList(-2), player1, player2,     false)
-                )
-    }
+		return Math.max(Math.max.apply(Math, take1Array),
+						Math.max.apply(Math, take2Array),
+						Math.max.apply(Math, take2Array),
+						Math.max.apply(Math, split2Array),
+						Math.max.apply(Math, split3Array)
+				
+		)
+	}else{
+		return Math.min(Math.min.apply(Math, take1Array),
+						Math.min.apply(Math, take2Array),
+						Math.min.apply(Math, take2Array),
+						Math.min.apply(Math, split2Array),
+						Math.min.apply(Math, split3Array)
+		)
+	}
+		
+} 
+
+function robot_step(take_button){
+		
+	switch(take_button){
+		case 1:
+			document.querySelector(`[id*="one"]`).remove();
+			player2 = player2 + 1;
+		break;
+		
+		case 2:
+			document.querySelector(`[id*="two"]`).remove();
+			player2 = player2 + 2;
+		break;
+		
+		case 3:
+			document.querySelector(`[id*="tre"]`).remove();
+			player2 = player2 + 3;
+		break;
+	}
+	active_player = 'user';
+	console.log('Robot scored ' + take_button + ' points');
+	document.getElementById("playerScore2").innerHTML = "Robot: " + player2 + " points";
+	change_color();
+	gemeOver();
 }
 
-function getButtonList(action) {
-
-    /* Action parametr description
-     * 3  - player took 3 points
-     * 2  - player took 2 points
-     * 1  - player took 1 point
-     * -3 - split the 3 into 2 elements (1 and 2)
-     * -2 - split the 2 into 2 elements (1 and 1)
-     */
+function getButtonList() {
 
     var elements = document.querySelectorAll("button");
     buttonList = [];
     for (var i = 0, len = elements.length; i < len; i++) {
         buttonList.push(elements[i].textContent);
     }
-    switch (action) {
-        case 3:
-            if (buttonList.indexOf('3') > -1) {
-                buttonList.splice(buttonList.indexOf('3'), 1);
-                document.querySelector(`[id*="tre"]`).remove();
-            }
-            break;
-        case 2:
-            if (buttonList.indexOf('2') > -1) {
-                buttonList.splice(buttonList.indexOf('2'), 1);
-                document.querySelector(`[id*="two"]`).remove();
-            }
-            break;
-        case 1:
-            if (buttonList.indexOf('1') > -1) {
-                buttonList.splice(buttonList.indexOf('1'), 1);
-                document.querySelector(`[id*="one"]`).remove();
-            }
-            break;
-        case -3:
-            if (buttonList.indexOf('3') > -1) {
 
-                document.querySelector(`[id*="tre"]`).remove();
-
-                let btn1 = document.createElement("button");
-                btn1.innerHTML = '1';
-                btn1.setAttribute('id', 'one8');
-                btn1.classList = 'btn btn-success btn-lg m-1';
-                btn1.setAttribute('onclick', "update_score('one8')");
-                document.getElementById('button_group').appendChild(btn1)
-
-                let btn2 = document.createElement("button");
-                btn2.innerHTML = '2';
-                btn2.setAttribute('id', 'two4');
-                btn2.classList = 'btn btn-danger btn-lg';
-                btn2.setAttribute('onclick', "update_score('two4')");
-                document.getElementById('button_group').appendChild(btn2)
-            }
-            break;
-        case -2:
-            if (buttonList.indexOf('2') > -1) {
-
-                document.querySelector(`[id*="two"]`).remove();
-
-                let btn1 = document.createElement("button");
-                btn1.innerHTML = '1';
-                btn1.setAttribute('id', 'one6');
-                btn1.classList = 'btn btn-success btn-lg m-1';
-                btn1.setAttribute('onclick', "update_score('one6')");
-                document.getElementById('button_group').appendChild(btn1)
-
-                let btn2 = document.createElement("button");
-                btn2.innerHTML = '1';
-                btn2.setAttribute('id', 'one7');
-                btn2.classList = 'btn btn-success btn-lg';
-                btn2.setAttribute('onclick', "update_score('one7')");
-                document.getElementById('button_group').appendChild(btn2)
-            }
-            break;
-    }
-
-    buttonList = [];
-    for (var i = 0, len = elements.length; i < len; i++) {
-        buttonList.push(elements[i].textContent);
-    }
-
-    return buttonList.map(i => Number(i))
+    return buttonList.map(i => parseInt(i))
 }
